926aa674ba2754c649e4f373909505ef098845f6
lpj24 <pengjuli24@gmail.com>
2016-12-27 10:33:29 +0800

    fab

--- fabfile.py
+++ fabfile.py
@@ -8,45 +8,41 @@ env.hosts = ['192.168.182.166']
 env.user = 'lpj'
 env.password = '123'
 
-
-def test():
-    print os.path.pardir
-
-
-def make_targz_one_by_one(output_filename, source_dir):
-    tar = tarfile.open(output_filename, "w:gz")
-    for root, dirs, files in os.walk(source_dir):
-        for f in files:
-            file_type = f.split(".")
-            if len(file_type) == 2:
-                if file_type[1] == "sh":
-                    with lcd("D:\\前端\\新建文件夹\\bin"):
-                        run("dos2unix.exe " + f)
-                path_file = os.path.join(root, f)
-                tar.add(path_file)
-    tar.close()
-
-
-def make_tar_gz():
-    output_filename = "hbgj_statistics.tar.gz"
-    source_dir = os.path.join(os.path.pardir, "hbgj_statistics")
-    make_targz_one_by_one(output_filename, source_dir)
-    # with tarfile.open("hbgj_statistics.tar.gz", "w:gz") as tar:
-    #     tar.add("C:\\Users\\Administrator\\PycharmProjects\\hbgj_statistics",
-    #             arcname=os.path.basename("C:\\Users\\Administrator\\PycharmProjects\\hbgj_statistics"))
-
-
-def upload_file():
-    print run("ifconfig")
-    with cd("/home/lpj"):
-        result = put('hbgj_statistics.tar.gz', '/home/lpj/Public')
-        if result.failed:
-            print "put error"
+#
+# def test():
+#     print os.path.pardir
+#
+#
+# def make_targz_one_by_one(output_filename, source_dir):
+#     tar = tarfile.open(output_filename, "w:gz")
+#     for root, dirs, files in os.walk(source_dir):
+#         for f in files:
+#             file_type = f.split(".")
+#             if len(file_type) == 2:
+#                 if file_type[1] == "sh":
+#                     with lcd("D:\\前端\\新建文件夹\\bin"):
+#                         run("dos2unix.exe " + f)
+#                 path_file = os.path.join(root, f)
+#                 tar.add(path_file)
+#     tar.close()
+#
+#
+# def make_tar_gz():
+#     output_filename = "hbgj_statistics.tar.gz"
+#     source_dir = os.path.join(os.path.pardir, "hbgj_statistics")
+#     make_targz_one_by_one(output_filename, source_dir)
+#     # with tarfile.open("hbgj_statistics.tar.gz", "w:gz") as tar:
+#     #     tar.add("C:\\Users\\Administrator\\PycharmProjects\\hbgj_statistics",
+#     #             arcname=os.path.basename("C:\\Users\\Administrator\\PycharmProjects\\hbgj_statistics"))
 
 
-def go():
-    make_tar_gz()
-    upload_file()
+def git_file():
+    local("git add .")
+    local("git commit -m fab")
+    local("git push origin master")
+    # with cd("/home/lpj/Public/hbgj_statistics"):
+    #     run("git pull origin master")
+
 
 if __name__ == "__main__":
     # dest_path = 'hbgj_statistics.tar.gz'

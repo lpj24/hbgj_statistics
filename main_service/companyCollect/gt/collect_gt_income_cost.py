2d1f48581a0d2d80c60093a8c5a96965f24a2236
lpj24 <pengjuli24@gmail.com>
2017-01-12 14:33:28 +0800

    fab

--- main_service/companyCollect/gt/collect_gt_income_cost.py
+++ main_service/companyCollect/gt/collect_gt_income_cost.py
@@ -5,32 +5,39 @@ import datetime
 
 
 def collect_income_cost():
-    sql = """
-        select * from income_and_cost
+    query_sql = """
+        select *
+        from income_and_cost
     """
 
-    insert_income_sql = """
-        insert into profit_gt_income (s_day, type, amount) values (%s, %s, %s)
+    update_income_sql = """
+        insert into profit_gt_income (s_day, type, amount, createtime, updatetime)
+        values (%s, %s, %s, now(), now())
     """
 
-    insert_cost_sql = """
-        insert into profit_gt_cost (s_day, type, amount) values (%s, %s, %s)
+    update_cost_sql = """
+        insert into profit_gt_cost (s_day, type, amount, createtime, updatetime)
+        values (%s, %s, %s, now(), now())
     """
-    result_income_cost = DBCli(dict).gt_cli.queryAll(sql)
+    result_income_cost = DBCli(dict).gt_cli.queryAll(query_sql)
 
+    income_cost_type = DBCli().gt_cli.queryAll("select COLUMN_NAME from information_schema.COLUMNS "
+                                               "where table_name = 'income_and_cost'")
+    income_cost_type = [i[0] for i in income_cost_type]
+    income_cost_type.pop(0)
+    income_cost_type.pop(-1)
+    income_cost_type.pop(-1)
+    # income_cost_type.reverse()
     for result in result_income_cost:
         s_day = result["s_date"]
-        for k, v in result.items():
-            if k.startswith("in"):
-                if not v:
-                    v = 0
-                dto = [s_day, k, v]
-                DBCli().targetdb_cli.insert(insert_income_sql, dto)
-            elif k.startswith("cost"):
-                if not v:
-                    v = 0
-                dto = [s_day, k, v]
-                DBCli().targetdb_cli.insert(insert_cost_sql, dto)
+
+        for i_c_type in income_cost_type:
+            v = result[i_c_type] if result.get(i_c_type, None) else 0
+            dto = [s_day, i_c_type, v]
+            if i_c_type.startswith("in"):
+                DBCli().targetdb_cli.insert(update_income_sql, dto)
+            elif i_c_type.startswith("cost"):
+                DBCli().targetdb_cli.insert(update_cost_sql, dto)
 
 if __name__ == "__main__":
     collect_income_cost()
--- main_service/gt/gt_income_cost.py
+++ main_service/gt/gt_income_cost.py
@@ -8,7 +8,8 @@ def update_gt_income_cost(days):
     end_date = DateUtil.get_date_before_days(0)
 
     query_sql = """
-        select * from income_and_cost where s_date >= %s and s_date < %s
+        select *
+        from income_and_cost where s_date >= %s and s_date < %s
     """
 
     update_income_sql = """
@@ -22,21 +23,38 @@ def update_gt_income_cost(days):
     """
     query_dto = [start_date, end_date]
     result_income_cost = DBCli(dict).gt_cli.queryAll(query_sql, query_dto)
+
+    income_cost_type = DBCli().gt_cli.queryAll("select COLUMN_NAME from information_schema.COLUMNS "
+                                               "where table_name = 'income_and_cost'")
+    income_cost_type = [i[0] for i in income_cost_type]
+    income_cost_type.pop(0)
+    income_cost_type.pop(-1)
+    income_cost_type.pop(-1)
+    # income_cost_type.reverse()
     for result in result_income_cost:
         s_day = result["s_date"]
         exists_income = DBCli().targetdb_cli.queryOne("select * from profit_gt_income where s_day=%s", [s_day])
         exists_cost = DBCli().targetdb_cli.queryOne("select * from profit_gt_cost where s_day=%s", [s_day])
-        for k, v in result.items():
-            if k.startswith("in") and not exists_income:
-                if not v:
-                    v = 0
-                dto = [s_day, k, v]
+
+        for i_c_type in income_cost_type:
+            v = result[i_c_type] if result.get(i_c_type, None) else 0
+            dto = [s_day, i_c_type, v]
+            if i_c_type.startswith("in") and not exists_income:
                 DBCli().targetdb_cli.insert(update_income_sql, dto)
-            elif k.startswith("cost") and not exists_cost:
-                if not v:
-                    v = 0
-                dto = [s_day, k, v]
+            elif i_c_type.startswith("cost") and not exists_cost:
                 DBCli().targetdb_cli.insert(update_cost_sql, dto)
+
+        # for k, v in result.items():
+        #     if k.startswith("in") and not exists_income:
+        #         if not v:
+        #             v = 0
+        #         dto = [s_day, k, v]
+        #         DBCli().targetdb_cli.insert(update_income_sql, dto)
+        #     elif k.startswith("cost") and not exists_cost:
+        #         if not v:
+        #             v = 0
+        #         dto = [s_day, k, v]
+        #         DBCli().targetdb_cli.insert(update_cost_sql, dto)
 
 if __name__ == "__main__":
     update_gt_income_cost(1)

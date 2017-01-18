24ceb99ecef3c4ef48497089f719883ca33509a3
lpj24 <pengjuli24@gmail.com>
2017-01-18 10:08:18 +0800

    fab

--- main_service/hb/hb_profit_cost.py
+++ main_service/hb/hb_profit_cost.py
@@ -5,7 +5,8 @@ from dbClient.dateutil import DateUtil
 
 
 def update_hb_car_hotel_profit(days=0):
-    query_date = DateUtil.get_date_before_days(days)
+    query_date = DateUtil.get_date_before_days(days * 3)
+    today = DateUtil.get_today()
     sql = """
         select distinct TRADE_TIME s_day,
         sum(case when (AMOUNT_TYPE=2 and PRODUCT='0' and TRADE_CHANNEL not like '%%coupon%%') then amount else 0 end) paycost_in,
@@ -15,19 +16,28 @@ def update_hb_car_hotel_profit(days=0):
         sum(case when (AMOUNT_TYPE=6 and PRODUCT='20') then amount else 0 end) delay_care,
         sum(case when (AMOUNT_TYPE=5 and PRODUCT in ('1','2','3','4','9','10','12','14')) then amount else 0 end) point_give_amount,
         sum(case when (AMOUNT_TYPE=6 and PRODUCT in ('6','8','24','25')) then amount else 0 end) balance_give_amount
-        from PAY_COST_INFO where TRADE_TIME=%s
+        from PAY_COST_INFO where TRADE_TIME>=%s and TRADE_TIME<%s
         group by TRADE_TIME
     """
-    dto = [query_date]
-    result = DBCli().pay_cost_cli.queryOne(sql, dto)
+    dto = [query_date, today]
+    result = DBCli().pay_cost_cli.queryAll(sql, dto)
 
     insert_sql = """
         insert into profit_hb_cost (s_day, paycost_in, paycost_return, coupon_in, coupon_return,
         delay_care, point_give_amount, balance_give_amount, createtime, updatetime) values (
             %s, %s, %s, %s, %s, %s, %s, %s, now(), now()
         )
+        on duplicate key update updatetime = now(),
+        s_day = VALUES(s_day),
+        paycost_in = VALUES(paycost_in),
+        paycost_return = VALUES(paycost_return),
+        coupon_in = VALUES(coupon_in),
+        coupon_return = VALUES(coupon_return),
+        delay_care = VALUES(delay_care),
+        point_give_amount = VALUES(point_give_amount),
+        balance_give_amount = VALUES(balance_give_amount)
     """
-    DBCli().targetdb_cli.insert(insert_sql, result)
+    DBCli().targetdb_cli.batchInsert(insert_sql, result)
 
     car_sql = """
         select distinct TRADE_TIME s_day,
@@ -37,18 +47,26 @@ def update_hb_car_hotel_profit(days=0):
         sum(case when (AMOUNT_TYPE=4 and PRODUCT='7' and TRADE_CHANNEL like '%%coupon%%') then amount else 0 end) coupon_return,
         sum(case when (AMOUNT_TYPE=5 and PRODUCT in ('5','13')) then amount else 0 end) point_give_amount,
         sum(case when (AMOUNT_TYPE=6 and PRODUCT in ('12','29')) then amount else 0 end) balance_give_amount
-        from PAY_COST_INFO where TRADE_TIME=%s
+        from PAY_COST_INFO where TRADE_TIME>=%s and TRADE_TIME<%s
         group by s_day
     """
 
-    result = DBCli().pay_cost_cli.queryOne(car_sql, dto)
+    result = DBCli().pay_cost_cli.queryAll(car_sql, dto)
     insert_car_sql = """
         insert into profit_huoli_car_cost (s_day, paycost_in, paycost_return, coupon_in, coupon_return,
          point_give_amount, balance_give_amount, createtime, updatetime) values (
             %s, %s, %s, %s, %s, %s, %s, now(), now()
         )
+        on duplicate key update updatetime = now(),
+        s_day = VALUES(s_day),
+        paycost_in = VALUES(paycost_in),
+        paycost_return = VALUES(paycost_return),
+        coupon_in = VALUES(coupon_in),
+        coupon_return = VALUES(coupon_return),
+        point_give_amount = VALUES(point_give_amount),
+        balance_give_amount = VALUES(balance_give_amount)
     """
-    DBCli().targetdb_cli.insert(insert_car_sql, result)
+    DBCli().targetdb_cli.batchInsert(insert_car_sql, result)
 
     hotel_sql = """
         select distinct TRADE_TIME s_day,
@@ -58,18 +76,26 @@ def update_hb_car_hotel_profit(days=0):
         sum(case when (AMOUNT_TYPE=4 and PRODUCT='36' and TRADE_CHANNEL like '%%coupon%%') then amount else 0 end) coupon_return,
         sum(case when (AMOUNT_TYPE=5 and PRODUCT in ('8')) then amount else 0 end) point_give_amount,
         sum(case when (AMOUNT_TYPE=6 and PRODUCT in ('9','10')) then amount else 0 end) balance_give_amount
-        from PAY_COST_INFO where TRADE_TIME=%s
+        from PAY_COST_INFO where TRADE_TIME>=%s and TRADE_TIME<%s
         group by s_day
     """
 
-    result = DBCli().pay_cost_cli.queryOne(hotel_sql, dto)
+    result = DBCli().pay_cost_cli.queryAll(hotel_sql, dto)
     insert_hotel_sql = """
         insert into profit_huoli_hotel_cost (s_day, paycost_in, paycost_return, coupon_in, coupon_return,
          point_give_amount, balance_give_amount, createtime, updatetime) values (
             %s, %s, %s, %s, %s, %s, %s, now(), now()
         )
+        on duplicate key update updatetime = now(),
+        s_day = VALUES(s_day),
+        paycost_in = VALUES(paycost_in),
+        paycost_return = VALUES(paycost_return),
+        coupon_in = VALUES(coupon_in),
+        coupon_return = VALUES(coupon_return),
+        point_give_amount = VALUES(point_give_amount),
+        balance_give_amount = VALUES(balance_give_amount)
     """
-    DBCli().targetdb_cli.insert(insert_hotel_sql, result)
+    DBCli().targetdb_cli.batchInsert(insert_hotel_sql, result)
 
 
 if __name__ == "__main__":

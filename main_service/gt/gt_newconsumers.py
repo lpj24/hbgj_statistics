4936221c6d65eb2429c351d908693ebace2562b3
lpj24 <pengjuli24@gmail.com>
2017-02-28 15:09:22 +0800

    fab

--- main_service/gt/gt_newconsumers.py
+++ main_service/gt/gt_newconsumers.py
@@ -88,12 +88,12 @@ def gt_newconsumers_daily(days=0):
     redis_cli.delete("today_uid_android")
 
 
-def gt_newconsumers_hourly(s_hour):
+def gt_newconsumers_hourly():
     redis_cli = DBCli().redis_cli
     gt_cli = DBCli().gt_cli
     targetdb_cli = DBCli().targetdb_cli
     s_day = DateUtil.get_today("%Y-%m-%d")
-    # s_hour = int(datetime.datetime.now().strftime("%H"))
+    s_hour = int(datetime.datetime.now().strftime("%H"))
 
     if s_hour == 0:
         s_day = DateUtil.date2str(DateUtil.get_date_before_days(1), '%Y-%m-%d')
@@ -104,7 +104,6 @@ def gt_newconsumers_hourly(s_hour):
     query_start_date = s_day + " " + str(s_hour) + ":00:00"
     query_end_date = s_day + " " + str(s_hour) + ":59:59"
     dto = [query_start_date, query_end_date]
-    print dto
     hourly_sql_ios = """
         SELECT distinct uid
                   FROM user_order
@@ -166,5 +165,5 @@ if __name__ == "__main__":
     # gt_newconsumers_history()
     i = 8
     while i <= 15:
-        gt_newconsumers_hourly(i)
+        gt_newconsumers_hourly()
         i += 1

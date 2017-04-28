176b109b058cbd9967402d9251a774407f127010
lpj24 <pengjuli24@gmail.com>
2017-04-28 10:28:48 +0800

    fab

--- main_service/gt/gt_income_cost.py
+++ main_service/gt/gt_income_cost.py
@@ -16,6 +16,10 @@ def update_gt_income_cost(days):
     insert_income_sql = """
         insert into profit_gt_income (s_day, type, amount, createtime, updatetime)
         values (%s, %s, %s, now(), now())
+        on duplicate key update updatetime = now() ,
+        s_day = values(s_day),
+        type = values(type),
+        amount = values(amount)
     """
 
     insert_cost_sql = """
@@ -25,14 +29,6 @@ def update_gt_income_cost(days):
         s_day = values(s_day),
         type = values(type),
         amount = values(amount)
-    """
-
-    update_income_sql = """
-        update profit_gt_income set amount=%s, updatetime=now() where s_day = %s and type=%s
-    """
-
-    update_cost_sql = """
-        update profit_gt_cost set amount=%s, updatetime=now() where s_day = %s and type=%s
     """
 
     gt_coupon_use_sql = """
@@ -56,27 +52,16 @@ def update_gt_income_cost(days):
     income_cost_type.pop(-1)
     income_cost_type.pop(-1)
 
-    # income_cost_type.reverse()
     for result in result_income_cost:
         s_day = result["s_date"]
-        exists_income = DBCli().targetdb_cli.queryOne("select * from profit_gt_income where s_day=%s", [s_day])
-        exists_cost = DBCli().targetdb_cli.queryOne("select * from profit_gt_cost where s_day=%s", [s_day])
-
         for i_c_type in income_cost_type:
             v = result[i_c_type] if result.get(i_c_type, None) else 0
+
             dto = [s_day, i_c_type, v]
             if i_c_type.startswith("in"):
-                if not exists_income:
-                    DBCli().targetdb_cli.insert(insert_income_sql, dto)
-                else:
-                    dto = [v, s_day, i_c_type]
-                    DBCli().targetdb_cli.insert(update_income_sql, dto)
+                DBCli().targetdb_cli.insert(insert_income_sql, dto)
             elif i_c_type.startswith("cost"):
-                if not exists_cost:
-                    DBCli().targetdb_cli.insert(insert_cost_sql, dto)
-                else:
-                    dto = [v, s_day, i_c_type]
-                    DBCli().targetdb_cli.insert(update_cost_sql, dto)
+                DBCli().targetdb_cli.insert(insert_cost_sql, dto)
     gt_coupon_data = DBCli(dict).pay_cost_cli.queryAll(gt_coupon_use_sql, query_dto)
     insert_gt_coupon = []
     for gt_coupon in gt_coupon_data:
---
+++ tmp_py.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from main_service.gt.gt_income_cost import update_gt_income_cost
+if __name__ == '__main__':
+	update_gt_income_cost(1)

a07cd52e05c6559960a5dc4e54180abef1248cda
lpj24 <pengjuli24@gmail.com>
2017-05-04 09:57:30 +0800

    fab

--- dbClient/dbUtils.py
+++ dbClient/dbUtils.py
@@ -43,7 +43,7 @@ class DButils(object):
         try:
             logging.warning(self.log_str(sql, params))
             cursor.execute(sql, params)
-            logging.warning(dir(cursor))
+            # logging.warning(dir(cursor))
             self._conn.commit()
         except MySQLdb.Error, e:
             warning_time = time.strftime('%Y-%m-%d %H:%M', time.localtime())
@@ -62,7 +62,7 @@ class DButils(object):
                 sql, num = rx.subn(params.pop(), sql)
             cursor.execute(sql, params)
 
-        logging.warning("execute sql" + dir(cursor))
+        # logging.warning("execute sql" + dir(cursor))
         data = cursor.fetchall()
         cursor.close()
         return data
@@ -77,7 +77,7 @@ class DButils(object):
                     rx = re.compile("tablename")
                     sql, num = rx.subn(params.pop(), sql)
                 cursor.execute(sql, params)
-            # logging.warning("execute sql" + cursor._executed)
+            # logging.warning(dir(cursor._executed))
             data = cursor.fetchone()
         except MySQLdb.OperationalError:
             logging.warning("error")

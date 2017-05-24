3f2db904ea98c441d4f79e505a418f392fb3404e
lpj24 <pengjuli24@gmail.com>
2017-05-24 08:59:30 +0800

    fab

--- monitor/monitor_sql.py
+++ monitor/monitor_sql.py
@@ -91,6 +91,7 @@ sql = [
     "select count(1) from profit_huoli_buy_cost where s_day=%s",
     "select count(1) from hbgj_newusers_daily where s_day=%s",
     "select count(1) from hbdt_focus_platform_daily where s_day=%s",
+    "select count(1) from hbgj_order_detail_daily_new where s_day=%s",
 ]
 
 week_sql = [

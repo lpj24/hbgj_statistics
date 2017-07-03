4f901a86a8dfb4e0b58e30c016deb8b09c49a8ca
lpj24 <pengjuli24@gmail.com>
2017-07-03 11:40:46 +0800

    fab

--- main_service/hb/hb_activeusers.py
+++ main_service/hb/hb_activeusers.py
@@ -71,6 +71,7 @@ if __name__ == "__main__":
     #     update_hbgj_activeusers_daily(i)
     #     i -= 1
     # update_hbgj_activeusers_daily(1)
+
     update_hbgj_activeusers_monthly()
     # i = 33
     # while i >= 1:

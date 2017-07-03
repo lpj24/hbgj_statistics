3728b1c36b55eb421942b9ba414eee7499993f71
lpj24 <pengjuli24@gmail.com>
2017-07-03 14:09:46 +0800

    fab

--- main_service/hb/hb_activeusers.py
+++ main_service/hb/hb_activeusers.py
@@ -18,8 +18,8 @@ def update_hbgj_activeusers_weekly():
     start_date, end_date = DateUtil.get_last_week_date()
     start_date = DateUtil.date2str(start_date, '%Y-%m-%d')
     end_date = DateUtil.date2str(end_date, '%Y-%m-%d')
-    dto = {"start_date": start_date, "end_date": end_date}
-    query_data = DBCli().oracle_cli.queryOne(hb_activeusers_sql["hbgj_activeusers_weekly"], dto)
+    dto = [start_date, end_date]
+    query_data = DBCli().apibase_cli.queryOne(hb_activeusers_sql["hbgj_activeusers_weekly"], dto)
     DBCli().targetdb_cli.insert(hb_activeusers_sql["update_hbgj_activeusers_weekly"], query_data)
 
 
@@ -27,8 +27,8 @@ def update_hbgj_activeusers_monthly():
     start_date, end_date = DateUtil.get_last_month_date()
     start_date = DateUtil.date2str(start_date, '%Y-%m-%d')
     end_date = DateUtil.date2str(end_date, '%Y-%m-%d')
-    dto = {"start_date": start_date, "end_date": end_date}
-    query_data = DBCli().oracle_cli.queryOne(hb_activeusers_sql["hbgj_activeusers_monthly"], dto)
+    dto = [start_date, end_date]
+    query_data = DBCli().apibase_cli.queryOne(hb_activeusers_sql["hbgj_activeusers_monthly"], dto)
     DBCli().targetdb_cli.insert(hb_activeusers_sql["update_hbgj_activeusers_monthly"], query_data)
 
 
@@ -71,7 +71,10 @@ if __name__ == "__main__":
     #     update_hbgj_activeusers_daily(i)
     #     i -= 1
     # update_hbgj_activeusers_daily(1)
-    update_hbgj_activeusers_monthly()
+
+    update_hbgj_activeusers_weekly()
+
+    # update_hbgj_activeusers_monthly()
 
 
     # i = 33
--- sql/hbsql/hb_activeusers.py
+++ sql/hbsql/hb_activeusers.py
@@ -21,26 +21,23 @@ update_hbgj_activeusers_daily = """
 """
 
 hbgj_activeusers_weekly = """
-select A.s_day,A.active_users, A.active_users_ios,
- (A.active_users-A.active_users_ios) active_users_android FROM
-(
-select to_char(TRUNC(createtime,'IW'),'yyyy-mm-dd') s_day,
+    select A.s_day,A.active_users, A.active_users_ios,
+    (A.active_users-A.active_users_ios) active_users_android
+    from (select DATE_FORMAT(subdate(createtime, weekday(createtime)), '%%Y-%%m-%%d') s_day,
 count(distinct userid) active_users,
-count(distinct case when p LIKE '%%91ZS%%' or p LIKE '%%appstore%%'
-or p LIKE '%%juwan%%' or p LIKE '%%91PGZS%%' or p LIKE '%%kuaiyong%%'
-or p LIKE '%%TBT%%' or p LIKE '%%PPZS%%'
-then userid else null end ) active_users_ios
-from ACTIVE_USER_LOG
-where createtime>=to_date(:start_date, 'yyyy-mm-dd')
-and createtime<to_date(:end_date, 'yyyy-mm-dd')
-and p like '%hbgj%'
-group by to_char(TRUNC(createtime,'IW'),'yyyy-mm-dd')
-) A
+    count(distinct case when p LIKE '%%91ZS%%' or p LIKE '%%appstore%%' or p LIKE '%%juwan%%' or p LIKE '%%91PGZS%%'
+    or p LIKE '%%kuaiyong%%' or p LIKE '%%TBT%%' or p LIKE '%%PPZS%%' then USERID else null end ) active_users_ios
+    from ACTIVE_USER_LOG
+    where DATE_FORMAT(createtime,'%%Y-%%m-%%d')>=%s
+    and DATE_FORMAT(createtime,'%%Y-%%m-%%d')<%s
+    and p like '%%hbgj%%'
+GROUP BY s_day
+) A;
 """
 
 update_hbgj_activeusers_weekly = """
     insert into hbgj_activeusers_weekly (s_day, active_users, active_users_ios , active_users_android,
-    createtime, updatetime) values (%s, %s , %s, %s, now(), now())
+    createtime, updatetime) values (%s, %s, %s, %s, now(), now())
     on duplicate key update updatetime = now(),
     active_users = VALUES(active_users),
     active_users_ios = VALUES(active_users_ios),
@@ -48,21 +45,18 @@ update_hbgj_activeusers_weekly = """
 """
 
 hbgj_activeusers_monthly = """
-select A.s_day,A.active_users, A.active_users_ios,
-(A.active_users-A.active_users_ios) active_users_android
-FROM(
-select to_char(trunc(createtime,'mm'),'yyyy-mm-dd') s_day,
+    select A.s_day,A.active_users, A.active_users_ios,
+    (A.active_users-A.active_users_ios) active_users_android
+    from (select DATE_FORMAT(subdate(createtime, day(createtime) - 1), '%%Y-%%m-%%d') s_day,
 count(distinct userid) active_users,
-count(distinct case when p LIKE '%%91ZS%%' or
-p LIKE '%%appstore%%' or p LIKE '%%juwan%%' or
-p LIKE '%%91PGZS%%' or p LIKE '%%kuaiyong%%' or
-p LIKE '%%TBT%%' or p LIKE '%%PPZS%%' then userid else null end ) active_users_ios
-from ACTIVE_USER_LOG
-where createtime>=to_date(:start_date, 'yyyy-mm-dd')
-and createtime<to_date(:end_date, 'yyyy-mm-dd')
-and p like '%hbgj%'
-group by to_char(trunc(createtime,'mm'),'yyyy-mm-dd')
-) A
+    count(distinct case when p LIKE '%%91ZS%%' or p LIKE '%%appstore%%' or p LIKE '%%juwan%%' or p LIKE '%%91PGZS%%'
+    or p LIKE '%%kuaiyong%%' or p LIKE '%%TBT%%' or p LIKE '%%PPZS%%' then USERID else null end ) active_users_ios
+    from ACTIVE_USER_LOG
+    where DATE_FORMAT(createtime,'%%Y-%%m-%%d')>=%s
+    and DATE_FORMAT(createtime,'%%Y-%%m-%%d')<%s
+    and p like '%%hbgj%%'
+GROUP BY s_day
+) A;
 """
 
 update_hbgj_activeusers_monthly = """

c78c375e838d8b8b02447e90e8f403a0cc7410cb
lpj24 <pengjuli24@gmail.com>
2017-07-13 09:09:42 +0800

    fab

--- dbClient/utils.py
+++ dbClient/utils.py
@@ -91,6 +91,7 @@ def execute_job_thread_pool(queue, arg):
     """
     for i in xrange(6):
         t = ThreadExecuteJob(queue, arg)
+
         t.setName("thread" + str(i))
         t.setDaemon(True)
         t.start()

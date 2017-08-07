04c0912b404dd1253132a0739767dad20f1840a4
lpj24 <pengjuli24@gmail.com>
2017-08-07 10:50:09 +0800

    fab

--- main_service/hb/hb_consumers.py
+++ main_service/hb/hb_consumers.py
@@ -130,10 +130,62 @@ def update_hbgj_consumers_inter_daily(days=0):
     query_data = DBCli().sourcedb_cli.queryAll(inter_sql, dto)
     DBCli().targetdb_cli.batchInsert(insert_sql, query_data)
 
+
+def update_hbgj_newconsumers_inter_daily_nation(days=0):
+    """国际机票新增消费用户购买过国内机票, hbgj_newconsumers_inter_daily_nation"""
+    today = DateUtil.date2str(DateUtil.get_date_before_days(int(days) * 1), '%Y-%m-%d')
+    tomorrow = DateUtil.date2str(DateUtil.get_date_after_days(1 - int(days)), '%Y-%m-%d')
+    dto = [today, tomorrow, today, today]
+    inter_sql = """
+        select A.s_day, A.consumers, A.consumers_ios, (A.consumers - A.consumers_ios) from (
+        SELECT DATE_FORMAT(od.CREATETIME, '%%Y-%%m-%%d') s_day,count(DISTINCT PHONEID) consumers,
+        count(distinct case when p LIKE '%%ios%%' then PHONEID else null end ) consumers_ios
+        FROM `TICKET_ORDERDETAIL` od
+        INNER JOIN `TICKET_ORDER` o on od.ORDERID=o.ORDERID
+        where od.CREATETIME>= %s
+        and od.CREATETIME < %s
+        and o.ORDERSTATUE NOT IN (0, 1, 11, 12, 2, 21, 3, 31)
+        AND IFNULL(od.`LINKTYPE`, 0) != 2
+        and INTFLAG=1
+        and phoneid not in (
+            SELECT distinct PHONEID
+            FROM `TICKET_ORDERDETAIL` od
+            INNER JOIN `TICKET_ORDER` o on od.ORDERID=o.ORDERID
+            where  o.ORDERSTATUE NOT IN (0, 1, 11, 12, 2, 21, 3, 31)
+            AND IFNULL(od.`LINKTYPE`, 0) != 2
+            and INTFLAG=1
+            and od.CREATETIME< %s
+        )
+        and phoneid in (
+            SELECT distinct PHONEID
+            FROM `TICKET_ORDERDETAIL` od
+            INNER JOIN `TICKET_ORDER` o on od.ORDERID=o.ORDERID
+            where  o.ORDERSTATUE NOT IN (0, 1, 11, 12, 2, 21, 3, 31)
+            AND IFNULL(od.`LINKTYPE`, 0) != 2
+            and INTFLAG=0
+            and od.CREATETIME<%s
+            )
+        ) A
+    """
+    insert_sql = """
+        insert into hbgj_newconsumers_inter_daily_nation (s_day, new_consumers, new_consumers_ios, new_consumers_android,
+        createtime, updatetime)
+        values
+        (%s, %s, %s, %s, now(), now())
+        on duplicate key update updatetime = now(),
+        s_day = values(s_day),
+        new_consumers = values(new_consumers),
+        new_consumers_ios = values(new_consumers_ios),
+        new_consumers_android = values(new_consumers_android)
+    """
+    query_data = DBCli().sourcedb_cli.queryAll(inter_sql, dto)
+    DBCli().targetdb_cli.batchInsert(insert_sql, query_data)
+    return __file__
+
 if __name__ == "__main__":
     i = 1
-    while i <= 10:
-        update_hbgj_newconsumers_inter_daily(i)
+    while i <= 1284:
+        update_hbgj_newconsumers_inter_daily_nation(i)
         i += 1
     # i = 1151
     # while i <= 100000:

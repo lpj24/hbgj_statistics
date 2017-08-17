26f0ce858fa317d47e8bd81f9cf2581ae60d8dc9
lpj24 <pengjuli24@gmail.com>
2017-08-17 18:08:20 +0800

    fab

--- main_service/gt/gt_newconsumers.py
+++ main_service/gt/gt_newconsumers.py
@@ -94,17 +94,15 @@ def gt_newconsumers_daily(days=0):
     redis_cli.delete("today_uid_android")
 
 
-def gt_newconsumers_hourly():
+def gt_newconsumers_hourly(s_hour):
     redis_cli = DBCli().redis_cli
     s_day = DateUtil.get_today("%Y-%m-%d")
-    s_hour = int(datetime.datetime.now().strftime("%H"))
-
-    # s_day = DateUtil.date2str(DateUtil.get_date_before_days(0), '%Y-%m-%d')
-    if s_hour == 0:
-        s_day = DateUtil.date2str(DateUtil.get_date_before_days(1), '%Y-%m-%d')
-        s_hour = 23
-    else:
-        s_hour -= 1
+    # s_hour = int(datetime.datetime.now().strftime("%H"))
+    # if s_hour == 0:
+    #     s_day = DateUtil.date2str(DateUtil.get_date_before_days(1), '%Y-%m-%d')
+    #     s_hour = 23
+    # else:
+    #     s_hour -= 1
 
     query_start_date = s_day + " " + str(s_hour) + ":00:00"
     query_end_date = s_day + " " + str(s_hour) + ":59:59"
@@ -166,8 +164,11 @@ def gt_newconsumers_hourly():
 if __name__ == "__main__":
     # gt_newconsumers_daily(1)
     # gt_newconsumers_history()
-    pass
-    gt_newconsumers_hourly()
+    s = 14
+    while s <= 18:
+        print s
+        gt_newconsumers_hourly(s)
+        s += 1
     # gt_newconsumers_hourly(0, 7)
     # gt_newconsumers_hourly(0, 8)
     # days = 6

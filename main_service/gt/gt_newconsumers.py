611489609ee7a72beafe7677207f4be97ccfa8e3
lpj24 <pengjuli24@gmail.com>
2017-08-17 18:09:44 +0800

    fab

--- main_service/gt/gt_newconsumers.py
+++ main_service/gt/gt_newconsumers.py
@@ -94,15 +94,15 @@ def gt_newconsumers_daily(days=0):
     redis_cli.delete("today_uid_android")
 
 
-def gt_newconsumers_hourly(s_hour):
+def gt_newconsumers_hourly():
     redis_cli = DBCli().redis_cli
     s_day = DateUtil.get_today("%Y-%m-%d")
-    # s_hour = int(datetime.datetime.now().strftime("%H"))
-    # if s_hour == 0:
-    #     s_day = DateUtil.date2str(DateUtil.get_date_before_days(1), '%Y-%m-%d')
-    #     s_hour = 23
-    # else:
-    #     s_hour -= 1
+    s_hour = int(datetime.datetime.now().strftime("%H"))
+    if s_hour == 0:
+        s_day = DateUtil.date2str(DateUtil.get_date_before_days(1), '%Y-%m-%d')
+        s_hour = 23
+    else:
+        s_hour -= 1
 
     query_start_date = s_day + " " + str(s_hour) + ":00:00"
     query_end_date = s_day + " " + str(s_hour) + ":59:59"

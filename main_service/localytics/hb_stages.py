cf8f3898057dca9a8b3edea3d46e6679f198582d
lpj24 <pengjuli24@gmail.com>
2017-08-29 14:35:23 +0800

    fab

--- main_service/localytics/hb_stages.py
+++ main_service/localytics/hb_stages.py
@@ -8,8 +8,7 @@ from core import *
 def update_hbgj_stages_daily(days=0):
     """更新分期付款localytics, weex_installment_pay_daily"""
 
-    query_date = DateUtil.date2str(DateUtil.get_date_before_days(days * 3), '%Y-%m-%d')
-    end_date = DateUtil.date2str(DateUtil.get_date_before_days(int(days) * 1), '%Y-%m-%d')
+    query_date = DateUtil.date2str(DateUtil.get_date_before_days(days * 1), '%Y-%m-%d')
     event_list = ['weex.installment.pay.start', 'weex.installment.activated', 'weex.installment.pay.success']
     insert_data = []
 
@@ -22,10 +21,17 @@ def update_hbgj_stages_daily(days=0):
             ios_weex_installment_pay_activated_uv,
             ios_weex_installment_pay_success_pv,
             ios_weex_installment_pay_success_uv,
+
+            android_weex_installment_pay_start_pv,
+            android_weex_installment_pay_start_uv,
+            android_weex_installment_pay_activated_pv,
+            android_weex_installment_pay_activated_uv,
+            android_weex_installment_pay_success_pv,
+            android_weex_installment_pay_success_uv,
             createtime,
             updatetime
         ) values (
-            %s, %s, %s, %s, %s, %s, %s, now(), now()
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, now(), now()
         )
         on duplicate key update updatetime = now(),
         s_day = values(s_day),
@@ -34,57 +40,89 @@ def update_hbgj_stages_daily(days=0):
         ios_weex_installment_pay_activated_pv = values(ios_weex_installment_pay_activated_pv),
         ios_weex_installment_pay_activated_uv = values(ios_weex_installment_pay_activated_uv),
         ios_weex_installment_pay_success_pv = values(ios_weex_installment_pay_success_pv),
-        ios_weex_installment_pay_success_uv = values(ios_weex_installment_pay_success_uv)
+        ios_weex_installment_pay_success_uv = values(ios_weex_installment_pay_success_uv),
+        android_weex_installment_pay_start_pv = values(android_weex_installment_pay_start_pv),
+        android_weex_installment_pay_start_uv = values(android_weex_installment_pay_start_uv),
+        android_weex_installment_pay_activated_pv = values(android_weex_installment_pay_activated_pv),
+        android_weex_installment_pay_activated_uv = values(android_weex_installment_pay_activated_uv),
+        android_weex_installment_pay_success_pv = values(android_weex_installment_pay_success_pv),
+        android_weex_installment_pay_success_uv = values(android_weex_installment_pay_success_uv)
     """
     insert_result = defaultdict(list)
-    for p in ['ios.', ]:
-        event_list = [p + e for e in event_list]
+    for p in ['ios.', '']:
         for e in event_list:
-            pv_data = request_pv(query_date, end_date, e, 'day')
-            uv_data = request_uv(query_date, end_date, e, 'day')
-            for pv_detail, uv_detail in zip(pv_data, uv_data):
-                insert_result[pv_detail['day']].append(pv_detail['sessions_per_event'])
-                insert_result[pv_detail['day']].append(uv_detail['users'])
+            pv_data = request_pv(query_date, query_date, p + e, 'day')
+            uv_data = request_uv(query_date, query_date, p + e, 'day')
 
+            if len(pv_data) > 1:
+                insert_result[query_date].append(pv_data[0]['sessions_per_event'])
+            else:
+                insert_result[query_date].append(0)
+
+            if len(uv_data) > 1:
+                insert_result[query_date].append(uv_data[0]['users'])
+            else:
+                insert_result[query_date].append(0)
     for k, v in insert_result.items():
         v.insert(0, k)
         insert_data.append(v)
+
     DBCli().targetdb_cli.batchInsert(insert_sql, insert_data)
     return __file__
 
 
 def update_weex_activated_type_daily(days=0):
     """更新分期付款localytics, weex_installment_activated_type_daily"""
-    event_list = ['ios.weex.installment.activated']
-    query_date = DateUtil.date2str(DateUtil.get_date_before_days(days * 3), '%Y-%m-%d')
-    end_date = DateUtil.date2str(DateUtil.get_date_before_days(int(days) * 1), '%Y-%m-%d')
+    event_list = ['weex.installment.activated']
+    query_date = DateUtil.date2str(DateUtil.get_date_before_days(days * 1), '%Y-%m-%d')
     insert_sql = """
         insert into weex_installment_activated_type_daily (
             s_day,
             ios_weex_installment_activated_installment_pv,
-            ios_weex_installment_activated_installment_uv,
             ios_weex_installment_activated_mylimit_pv,
+            ios_weex_installment_activated_installment_uv,
             ios_weex_installment_activated_mylimit_uv,
+
+            android_weex_installment_activated_installment_pv,
+            android_weex_installment_activated_mylimit_pv,
+            android_weex_installment_activated_installment_uv,
+            android_weex_installment_activated_mylimit_uv,
             createtime,
             updatetime
         ) values (
-            %s, %s, %s, %s, %s, now(), now()
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, now(), now()
         )
         on duplicate key update updatetime = now(),
         s_day = values(s_day),
         ios_weex_installment_activated_installment_pv = values(ios_weex_installment_activated_installment_pv),
-        ios_weex_installment_activated_installment_uv = values(ios_weex_installment_activated_installment_uv),
         ios_weex_installment_activated_mylimit_pv = values(ios_weex_installment_activated_mylimit_pv),
-        ios_weex_installment_activated_mylimit_uv = values(ios_weex_installment_activated_mylimit_uv)
+        ios_weex_installment_activated_installment_uv = values(ios_weex_installment_activated_installment_uv),
+        ios_weex_installment_activated_mylimit_uv = values(ios_weex_installment_activated_mylimit_uv),
+
+        android_weex_installment_activated_installment_pv = values(android_weex_installment_activated_installment_pv),
+        android_weex_installment_activated_mylimit_pv = values(android_weex_installment_activated_mylimit_pv),
+        android_weex_installment_activated_installment_uv = values(android_weex_installment_activated_installment_uv),
+        android_weex_installment_activated_mylimit_uv = values(android_weex_installment_activated_mylimit_uv)
     """
     insert_result = defaultdict(list)
     insert_data = []
-    for e in event_list:
-        pv_data = request_pv(query_date, end_date, e, 'day, a:type')
-        uv_data = request_uv(query_date, end_date, e, 'day, a:type')
-        for pv_detail, uv_detail in zip(pv_data, uv_data):
-            insert_result[pv_detail['day']].append(pv_detail['sessions_per_event'])
-            insert_result[pv_detail['day']].append(uv_detail['users'])
+    for p in ['ios.', '']:
+        for e in event_list:
+            pv_data = request_pv(query_date, query_date, p + e, 'day, a:type')
+            uv_data = request_uv(query_date, query_date, p + e, 'day, a:type')
+            if len(pv_data) > 1:
+                insert_result[query_date].append(pv_data[0]['sessions_per_event'])
+                insert_result[query_date].append(pv_data[1]['sessions_per_event'])
+            else:
+                insert_result[query_date].append(0)
+                insert_result[query_date].append(0)
+
+            if len(uv_data) > 1:
+                insert_result[query_date].append(uv_data[0]['users'])
+                insert_result[query_date].append(uv_data[1]['users'])
+            else:
+                insert_result[query_date].append(0)
+                insert_result[query_date].append(0)
 
     for k, v in insert_result.items():
         v.insert(0, k)
@@ -94,4 +132,7 @@ def update_weex_activated_type_daily(days=0):
 
 if __name__ == "__main__":
     # update_hbgj_stages_daily(1)
-    update_weex_activated_type_daily(1)
+    i = 27
+    while i >= 1:
+        update_weex_activated_type_daily(i)
+        i -= 1

04ecc481cee693a1bdae9e7a7d8f79487fbd9656
lpj24 <lipenju24@163.com>
2017-10-17 09:34:06 +0800

    fab

--- monitor/task.py
+++ monitor/task.py
@@ -125,6 +125,7 @@ def check_execute_job():
 
 
 if __name__ == "__main__":
+    import subprocess
     later_service = execute_later_job()
     for fun in later_service.get_later_service():
         try:
@@ -138,3 +139,5 @@ if __name__ == "__main__":
     exception_table = cal_balance()
     if exception_table:
         utils.sendMail("762575190@qq.com", '<br/>'.join([t for t in exception_table]), "与前一天的数据有差异")
+    subprocess.Popen("ps -A -opid, etime, args | grep excute_tmp_day*| grep -v grep|awk '{print $1}'|xargs kill -9",
+                     stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)

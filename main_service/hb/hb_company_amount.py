ba40dd9360d92c5c93fa2b3531827939121f8266
lpj24 <lipenju24@163.com>
2018-03-23 15:35:26 +0800

    bf3753de-2e6c-11e8-9570-005056c00009

--- main_service/hb/hb_company_amount.py
+++ main_service/hb/hb_company_amount.py
@@ -283,7 +283,7 @@ def update_operation_hbgj_special_return_daily(days=1):
 
 def update_hb_company_income_cost_daily(days=0):
     """更新各个航空公司收入 成本 销售额 机票数量, hb_company_income_cost_daily"""
-    start_date = DateUtil.date2str(DateUtil.get_date_before_days(days*1))
+    start_date = DateUtil.date2str(DateUtil.get_date_before_days(days*81))
     end_date = DateUtil.date2str(DateUtil.get_date_after_days(1-days))
     dto = [start_date, end_date]
     income_sql = """

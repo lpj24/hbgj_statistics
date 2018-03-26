8ec21c651627d70965a940e7e924462df6fc3a9c
lpj24 <lipenju24@163.com>
2018-03-26 11:21:17 +0800

    bb28241e-30a4-11e8-b861-005056c00009

--- time_job_excute/excute_tmp_day.py
+++ time_job_excute/excute_tmp_day.py
@@ -63,6 +63,7 @@ def add_execute_job():
 
     TimeService.add_hard_service(hb_profit_cost.update_hb_inter_coupon_cost_daily)
     TimeService.add_hard_service(hb_company_amount.update_hb_company_income_cost_daily)
+    TimeService.add_hard_service(hb_company_amount.update_hb_company_income_cost_supplier_daily)
     return TimeService
 
 

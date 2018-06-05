b4606652ebebb13dfb1c728fb05d3c78f49c0255
lpj24 <lipenju24@163.com>
2018-06-05 17:12:46 +0800

    96deccc0-68a0-11e8-aa40-005056c00009

--- main_service/gt/gt_neworder.py
+++ main_service/gt/gt_neworder.py
@@ -5,8 +5,8 @@ from dbClient.dateutil import DateUtil
 
 def update_gt_order_daily(days=0):
     if days > 0:
-        start_date = DateUtil.get_date_before_days(3)
-        end_date = DateUtil.get_date_after_days(0)
+        start_date = DateUtil.get_date_before_days(25)
+        end_date = DateUtil.get_date_after_days(-7)
     else:
         start_date = DateUtil.get_date_before_days(days)
         end_date = DateUtil.add_days(start_date, 1)
@@ -14,10 +14,10 @@ def update_gt_order_daily(days=0):
     for x in xrange(2):
         dto.append(DateUtil.date2str(start_date))
         dto.append(DateUtil.date2str(end_date))
-
+    print dto
     query_data = DBCli().gt_cli.query_all(gt_new_order_sql["gt_neworder_daily"], dto)
     DBCli().targetdb_cli.batch_insert(gt_new_order_sql["update_gtgj_new_order_daily"], query_data)
 
 if __name__ == "__main__":
-    update_gt_order_daily()
+    update_gt_order_daily(1)
     # update_gt_order_hourly()

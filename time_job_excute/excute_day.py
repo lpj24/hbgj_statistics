1e04d0c38bbf5bcde89a522bff08c5def039135a
lpj24 <lipenju24@163.com>
2018-08-03 15:46:21 +0800

    4e7cda0f-96f1-11e8-8a78-005056c00009

--- time_job_excute/excute_day.py
+++ time_job_excute/excute_day.py
@@ -71,7 +71,6 @@ def add_execute_job():
     TimeService.add_day_service(hb_channel_client.update_hbgj_channel_client_ticket_daily)
     TimeService.add_day_service(hb_channel_client.update_hbgj_channel_client_ticket_h5_daily)
     TimeService.add_day_service(hb_insure.update_insure_type_detail_daily)
-
     TimeService.add_day_service(hb_coupon_email.send_hb_coupon_delay_eamil_daily)
     return TimeService
 
